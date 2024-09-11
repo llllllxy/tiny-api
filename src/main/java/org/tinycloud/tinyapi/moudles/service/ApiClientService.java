24849227cfd895ac995b1a3bd208d2b019c03fd8
llllllxy <34022230+llllllxy@users.noreply.github.com>
2024-09-11 22:53:32 +0800

    优化代码，

---
+++ src/main/java/org/tinycloud/tinyapi/moudles/service/ApiClientService.java
@@ -0,0 +1,14 @@
+package org.tinycloud.tinyapi.moudles.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author liuxingyu01
+ * @since 2024-09-2024/9/11 22:36
+ */
+@Service
+public class ApiClientService {
+}
---
+++ src/main/java/org/tinycloud/tinyapi/moudles/web/ApiClientController.java
@@ -0,0 +1,20 @@
+package org.tinycloud.tinyapi.moudles.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author liuxingyu01
+ * @since 2024-09-2024/9/11 22:35
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiClientController {
+
+
+
+
+}
