c01f48d144c23cca1f0cbbdc23fe1af832b9c4ea
llllllxy <34022230+llllllxy@users.noreply.github.com>
2024-09-13 13:31:25 +0800

    fixed: 补充代码

---
+++ src/main/java/org/tinycloud/tinyapi/common/config/interceptor/TenantAuthInterceptor.java
@@ -0,0 +1,99 @@
+package org.tinycloud.tinyapi.common.config.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.tinycloud.tinyapi.common.config.ApplicationConfig;
+import org.tinycloud.tinyapi.common.constant.GlobalConstant;
+import org.tinycloud.tinyapi.common.enums.TenantErrorCode;
+import org.tinycloud.tinyapi.common.exception.TenantException;
+import org.tinycloud.tinyapi.common.utils.JacksonUtils;
+import org.tinycloud.tinyapi.common.utils.StrUtils;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 租户会话拦截器
+ *
+ * @author liuxingyu01
+ * @since 2022-03-26 20:48
+ **/
+@Slf4j
+@Component
+public class TenantAuthInterceptor implements HandlerInterceptor {
+
+    private static final Long MILLIS_MINUTE_TEN = 20 * 60 * 1000L;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private ApplicationConfig applicationConfig;
+
+    /*
+     * 进入controller层之前拦截请求
+     * 返回值：表示是否将当前的请求拦截下来  false：拦截请求，请求别终止。true：请求不被拦截，继续执行
+     * Object obj:表示被拦的请求的目标对象（controller中方法）
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // 判断请求类型，如果是OPTIONS，直接返回
+        String options = HttpMethod.OPTIONS.toString();
+        if (options.equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return true;
+        }
+
+        // 先判断token是否为空
+        String token = TenantAuthUtil.getToken(request);
+        if (StrUtils.isBlank(token)) {
+            throw new TenantException(TenantErrorCode.TENANT_NOT_LOGIN);
+        }
+        // 再判断token是否存在
+        String tenantInfoString = redisTemplate.opsForValue().get(GlobalConstant.TENANT_TOKEN_REDIS_KEY + token);
+        if (StrUtils.isBlank(tenantInfoString)) {
+            throw new TenantException(TenantErrorCode.TENANT_NOT_LOGIN);
+        }
+
+        // 再判断token是否合法
+        LoginTenant loginTenant = JacksonUtils.readValue(tenantInfoString, LoginTenant.class);
+        if (Objects.isNull(loginTenant)) {
+            throw new TenantException(TenantErrorCode.TENANT_NOT_LOGIN);
+        }
+        long expireTime = loginTenant.getLoginExpireTime();
+        long currentTime = System.currentTimeMillis();
+        if (expireTime - currentTime <= MILLIS_MINUTE_TEN) {
+            // 刷新会话缓存时长
+            loginTenant.setLoginExpireTime(currentTime + applicationConfig.getTenantAuthTimeout() * 1000);
+            redisTemplate.opsForValue().set(GlobalConstant.TENANT_TOKEN_REDIS_KEY + token, JacksonUtils.toJsonString(loginTenant) ,applicationConfig.getTenantAuthTimeout(), TimeUnit.SECONDS);
+        }
+        TenantHolder.setTenant(loginTenant);
+
+        // 合格不需要拦截，放行
+        return true;
+    }
+
+    /*
+     * 处理请求完成后视图渲染之前的处理操作
+     * 通过ModelAndView参数改变显示的视图，或发往视图的方法
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+    }
+
+    /*
+     * 视图渲染之后的操作
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) throws Exception {
+        TenantHolder.clearTenant();
+    }
+
+}
---
+++ src/main/java/org/tinycloud/tinyapi/common/config/interceptor/TenantAuthUtil.java
@@ -0,0 +1,55 @@
+package org.tinycloud.tinyapi.common.config.interceptor;
+
+
+import javax.servlet.http.HttpServletRequest;
+import org.tinycloud.tinyapi.common.constant.GlobalConstant;
+import org.tinycloud.tinyapi.common.utils.StrUtils;
+import org.tinycloud.tinyapi.common.utils.web.CookieUtils;
+import org.tinycloud.tinyapi.common.utils.web.ServletUtils;
+
+/**
+ * @author liuxingyu01
+ * @date 2022-09-06 10:27
+ * @description 租户会话工具类
+ **/
+public class TenantAuthUtil {
+
+
+    /**
+     *
+     * 获取租户会话token
+     *
+     * @param request HttpServletRequest
+     * @return String
+     */
+    public static String getToken(HttpServletRequest request) {
+        String token = "";
+        if (request != null) {
+            // 从请求中获取token，先从Header里取，取不到的话再从cookie里取（适配前后端分离的模式）
+            token = request.getHeader(GlobalConstant.TENANT_TOKEN_KEY);
+            if (StrUtils.isBlank(token)) {
+                token = CookieUtils.getCookie(request, GlobalConstant.TENANT_TOKEN_KEY);
+            }
+        }
+        return token;
+    }
+
+    /**
+     * 获取租户会话token
+     *
+     * @return String
+     */
+    public static String getToken() {
+        HttpServletRequest request = ServletUtils.getRequest();
+        String token = "";
+        if (request != null) {
+            // 从请求中获取token，先从Header里取，取不到的话再从cookie里取（适配前后端分离的模式）
+            token = request.getHeader(GlobalConstant.TENANT_TOKEN_KEY);
+            if (StrUtils.isBlank(token)) {
+                token = CookieUtils.getCookie(request, GlobalConstant.TENANT_TOKEN_KEY);
+            }
+        }
+        return token;
+    }
+
+}
